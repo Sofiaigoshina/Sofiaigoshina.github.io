b0359f4dbc2d95aec84e0cfc6ce1a8b4a2474262
Sofiaigoshina <144098118+Sofiaigoshina@users.noreply.github.com>
2023-11-20 22:57:06 +0300

    Delete Laba5 directory

--- Laba5/index.js
+++
@@ -1,8 +0,0 @@
-function calculateTotal() {
-    var quantity = document.getElementById('quantity').value;
-    var selectedProduct = document.getElementById('product');
-    var productPrice = selectedProduct.options[selectedProduct.selectedIndex].getAttribute('data-price');
-    var totalCost = quantity * productPrice;
-  
-    document.getElementById('result').innerHTML = 'Общая стоимость заказа: ' + totalCost.toFixed(2) + ' рублей';
-  }
